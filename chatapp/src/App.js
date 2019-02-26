f5176caad5b4765d06a2a1caecd12f05e07ab67d
CalWal <callumtrwallace@gmail.com>
2019-02-26 18:20:59 +1000

    Added MessageList component. Chatkit to be implemented.

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Test from "./components/Test"
+import MessageList from "./components/MessageList"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
     <Test />
+    <MessageList />
       </div>
     );
   }
