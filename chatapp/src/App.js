a751f414d1fdbfc16dddd103e5bff3f4a581b594
CalWal <callumtrwallace@gmail.com>
2019-02-28 17:46:17 +1000

    added Chatkit API

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chatkit from '@pusher/chatkit-client'
+
+import {tokenURL, instanceLocator} from "./config.js"
+
 
 import Test from "./components/Test"
 import MessageList from "./components/MessageList"
 
 class App extends Component {
+
+componentDidMount(){
+  const chatManager = new Chatkit.ChatManager({
+    instanceLocator,
+    userId: 'Callum',
+    tokenProvider: new Chatkit.TokenProvider({
+      url: tokenURL
+    })
+  })
+
+  chatManager.connect()
+  .then(currentUser =>{
+    currentUser.subscribeToRoom({
+      roomId:"19409457",
+      hooks: {
+        onNewMessage: message =>{
+          console.log("message.text:", message.text);
+        }
+      }
+    })
+  })
+
+}
+
   render() {
     return (
       <div className="App">
-    <Test />
     <MessageList />
       </div>
     );
---
+++ chatapp/src/config.js
@@ -0,0 +1,5 @@
+const tokenURL="https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b4c7574a-3d5c-4296-ad56-c0f1839627a1/token"
+
+const instanceLocator = "v1:us1:b4c7574a-3d5c-4296-ad56-c0f1839627a1"
+
+export {tokenURL, instanceLocator}
