ce537cdc1480cb5d2c6bd74118fd7251cb8c6ecf
CalWal <callumtrwallace@gmail.com>
2019-03-07 18:54:21 +1000

    Updates

---
+++ precache-manifest.2b0812283d14c9ec3479ae4111f57f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5fd7819440c6ae4dd5f",
+    "url": "/chatapp/static/js/runtime~main.c5fd7819.js"
+  },
+  {
+    "revision": "18fd38a757d06647701a",
+    "url": "/chatapp/static/js/main.18fd38a7.chunk.js"
+  },
+  {
+    "revision": "ffa6e7a345bc4e124b2f",
+    "url": "/chatapp/static/js/2.ffa6e7a3.chunk.js"
+  },
+  {
+    "revision": "18fd38a757d06647701a",
+    "url": "/chatapp/static/css/main.aca56168.chunk.css"
+  },
+  {
+    "revision": "4cff43ff8e6ae74e291acc578b9a9f52",
+    "url": "/chatapp/index.html"
+  }
+];
